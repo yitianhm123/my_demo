a843d71f37135b41dae0df302893d0ef4ec87228
huangman <huangman>
2021-08-17 11:01:30 +0800

    上传代码

---
+++ common-service/src/main/java/com/example/common/Response/ResponseResult.java
@@ -0,0 +1,38 @@
+package com.example.common.Response;
+
+
+
+public class ResponseResult<T> {
+
+    private String message;
+
+
+    private int code;
+
+
+    private T data;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
---
+++ sem-service-manager/src/main/java/com/example/sem/tempUtil/transferCode.java
@@ -0,0 +1,4 @@
+package com.example.sem.tempUtil;
+
+public class transferCode {
+}
