131027d7c68bf6ec8e54ee9b77deb3cc9259560e
huangman <huangman>
2021-08-19 09:33:59 +0800

    upload file

--- sem-service-manager/src/main/java/com/example/sem/tempUtil/transferCode.java
+++ sem-service-manager/src/main/java/com/example/sem/tempUtil/transferCode.java
@@ -1,20 +1,75 @@
 package com.example.sem.tempUtil;
 
-import java.io.File;
-import java.io.UnsupportedEncodingException;
+import com.example.sem.vo.UserVO;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectReader;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.sun.corba.se.spi.ior.ObjectKey;
+import jdk.nashorn.internal.runtime.JSONFunctions;
+import org.springframework.util.StringUtils;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Random;
 
 public class transferCode {
 
-    public static void main(String[] args) {
-        File file =new File("");
+    public static void main(String[] args) throws JsonProcessingException {
+//        File file =new File("");
+//
+//        String tem ="Temperature de conception (?)";
+//        try {
+//            byte[] bytes =tem.getBytes("text/tab-separated-values;charset=iso-8859-1");
+//            System.out.println(new String(bytes));
+//        } catch (UnsupportedEncodingException e) {
+//            e.printStackTrace();
+//        }
+//        System.out.println();‘
+        List<UserVO> userVOList =new ArrayList<>();
 
-        String tem ="Temperature de conception (?)";
+        for (int i = 0; i < 10; i++) {
+            Double  random = Math.random()*100;
+            UserVO vo=new UserVO();
+                    vo.setAddress(i+"sheng");
+                    vo.setName("huangman");
+                    vo.setAge(random.intValue());
+                    userVOList.add(vo);
+
+
+
+
+        }
+        ObjectMapper objectMapper = new ObjectMapper();
+              Optional<UserVO> userVO = userVOList.stream().filter(e->{return e.getName().equals("huangman");}).findAny();
+        ObjectWriter objectWriter = objectMapper.writer();
+        System.out.println(objectWriter.writeValueAsString(userVO.get()));
+
         try {
-            byte[] bytes =tem.getBytes("text/tab-separated-values;charset=iso-8859-1");
-            System.out.println(new String(bytes));
-        } catch (UnsupportedEncodingException e) {
+            BufferedWriter bufferedWriter =new BufferedWriter(new FileWriter(new File("temp_f")));
+            BufferedReader bufferedReader =new BufferedReader(new FileReader(new File("info.scp-inventory-service.log")));
+            String temp = "";
+            for (int i = 0; i < 10000000; i++) {
+
+           ;
+//                System.out.println(temp);
+                if(! StringUtils.isEmpty(temp = bufferedReader.readLine())&&temp.contains("推送PRM")){
+                    bufferedWriter.write(temp+"\n");
+                };
+
+            }
+
+            bufferedWriter.flush();
+            bufferedWriter.close();
+            bufferedReader.close();
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println();
     }
 }
---
+++ sem-service-manager/src/main/java/com/example/sem/vo/UserVO.java
@@ -0,0 +1,17 @@
+package com.example.sem.vo;
+
+
+import lombok.Data;
+
+@Data
+public class UserVO {
+
+    private String name;
+
+    private int age;
+
+    private String address;
+
+
+
+}
